63201482ffd7b12065f25ec6fe25275bd94d3930
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-07-18 17:42:56 +0500

    db fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const auth = require("./controllers/authorization");
 
 const db = knex({
   client: "pg",
-  connection: process.env.POSTGRES_URI
+  connection: { connectionString: process.env.DATABASE_URL, ssl: true }
 });
 
 const app = express();
